3c492788f61bade722fa7c5352d4f1a9776f9390
Student <student@students-macbook-pro.local>
2019-02-21 14:43:52 +0200

    login screen

--- FreestyleMeetApp/Controllers/ViewController.swift
+++ FreestyleMeetApp/Controllers/ViewController.swift
@@ -1,10 +1,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        initItself()
+        initLoginView()
+    }
+    
+    func initItself() {
+        self.view.setBackgroundImage(uiImage: #imageLiteral(resourceName: "ff_main_bg"))
+    }
+    
+    func initLoginView() {
+        loginView.clipsToBounds = true
+        
+        let layer = loginView.layer
+        layer.borderWidth = 1
+        layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        layer.cornerRadius = loginView.frame.size.width / 10
     }
 }
 
