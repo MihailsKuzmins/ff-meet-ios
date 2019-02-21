90e24ab63e725e0ba78c9837679727ea4c08b8b8
Student <student@students-macbook-pro.local>
2019-02-21 12:18:29 +0200

    Initial Commit

---
+++ FreestyleMeetApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FreestyleMeetApp
+//
+//  Created by Student on 21/02/2019.
+//  Copyright © 2019 kuzmimih. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
