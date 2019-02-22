cb3fb9a5ce079513d67e83c799abd82867ac55fc
Student <student@students-macbook-pro.local>
2019-02-22 16:36:57 +0200

    some files

---
+++ FreestyleMeetApp/Resources/CoreConstants.swift
@@ -0,0 +1,31 @@
+import Foundation
+import UIKit
+
+class CoreConstants {
+    class Storyboards {
+        public static let appStoryboard = "AppStoryboard"
+    }
+    
+    class ViewControllers {
+        public static let meetList = "MeetListViewController"
+        public static let appNavController = "AppStoryboardNavigationController"
+    }
+    
+    class CellIdentifiers {
+        public static let MeetTableViewCell = "meetTableViewCell"
+    }
+    
+    class DbKeys {
+        public static let meets = "meets"
+        public static let meetDate = "date"
+        public static let meetTime = "time"
+        public static let meetLatitude = "latitude"
+        public static let meetLocationName = "locationName"
+        public static let meetLongitude = "longitude"
+        public static let meetName = "name"
+    }
+    
+    class Ui {
+        public static let meetCellHeight: CGFloat = 100
+    }
+}
